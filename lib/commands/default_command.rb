9d3cfca3d3340018a53cf66b97650fd65c070a0d
mo k <mo@mokhan.ca>
2012-12-28 09:22:49 -0700

    read from a file to render the erb template.

--- lib/commands/default_command.rb
+++ lib/commands/default_command.rb
@@ -2,16 +2,8 @@ require 'erb'
 
 class DefaultCommand
   def run_against(request)
-
     time = Time.now
     name = "MO"
-    template = ERB.new <<-EOF
-      Oops... I couldn't find the page you were looking for! <%= time %>
-
-      <%= name %>
-    EOF
-
-    #[404, {"Content-Type" => "text/html"}, ["Oops... I couldn't find the page you were looking for! #{Time.now}"]]
-    [404, {"Content-Type" => "text/html"}, [template.result(binding)]]
+    [404, {"Content-Type" => "text/html"}, [ERB.new(File.read('lib/views/404.html.erb')).result(binding)]]
   end
 end
