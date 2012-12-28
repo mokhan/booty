a5f9b948d38813e38263aabecf74aac839eda8b1
mo k <mo@mokhan.ca>
2012-12-28 09:11:14 -0700

    use erb template to generate response body.

--- lib/commands/default_command.rb
+++ lib/commands/default_command.rb
@@ -1,5 +1,17 @@
+require 'erb'
+
 class DefaultCommand
   def run_against(request)
-    [404, {"Content-Type" => "text/html"}, ["Oops... I couldn't find the page you were looking for! #{Time.now}"]]
+
+    time = Time.now
+    name = "MO"
+    template = ERB.new <<-EOF
+      Oops... I couldn't find the page you were looking for! <%= time %>
+
+      <%= name %>
+    EOF
+
+    #[404, {"Content-Type" => "text/html"}, ["Oops... I couldn't find the page you were looking for! #{Time.now}"]]
+    [404, {"Content-Type" => "text/html"}, [template.result(binding)]]
   end
 end
