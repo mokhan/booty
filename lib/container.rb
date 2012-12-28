d924a5dd5e5aee2db84eb4252daf53b361918860
mo k <mo@mokhan.ca>
2012-12-28 15:56:42 -0700

    rename symbol to key

--- lib/container.rb
+++ lib/container.rb
@@ -3,14 +3,14 @@ class Container
     @items = {}
     register(:container) { self }
   end
-  def register(symbol, &block)
-    @items[symbol] = [] unless @items[symbol]
-    @items[symbol].push(block)
+  def register(key, &block)
+    @items[key] = [] unless @items[key]
+    @items[key].push(block)
   end
-  def resolve(symbol)
-    @items[symbol].first.call(self)
+  def resolve(key)
+    @items[key].first.call(self)
   end
-  def resolve_all(symbol)
-    @items[symbol].map {|item| item.call(self) }
+  def resolve_all(key)
+    @items[key].map {|item| item.call(self) }
   end
 end
