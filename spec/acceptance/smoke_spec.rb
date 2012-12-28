c81da31fbfb99fe0602915b7f448d67bb4a9473e
mo k <mo@mokhan.ca>
2012-12-28 08:58:20 -0700

    add watir smoke test.

---
+++ spec/acceptance/smoke_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'watir-webdriver'
+
+describe "blah" do
+  it "spec_name" do
+    b = Watir::Browser.new
+    b.goto 'bit.ly/watir-webdriver-demo'
+    b.text_field(:id => 'entry_0').set 'your name'
+    b.select_list(:id => 'entry_1').select 'Ruby'
+    b.select_list(:id => 'entry_1').selected? 'Ruby'
+    b.button(:name => 'submit').click
+    b.text.include? 'Thank you'
+  end
+end
