7ed62c2aa14b055d4abee8904c59caa4d9329788
mo k <mo@mokhan.ca>
2012-12-29 11:01:20 -0700

    move index command to dashboard namespace

--- lib/commands/dashboard/index_command.rb
+++ lib/commands/dashboard/index_command.rb
@@ -1,13 +1,15 @@
 module Booty
-  class IndexCommand 
-    def initialize
+  module Dashboard
+    class IndexCommand 
+      def initialize
 
-    end
-    def matches(request)
-      true
-    end
-    def run_against(request)
+      end
+      def matches(request)
+        true
+      end
+      def run_against(request)
 
+      end
     end
   end
 end
