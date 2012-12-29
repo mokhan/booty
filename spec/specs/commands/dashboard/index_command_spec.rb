5c2d673959baf511b05a4e908fe43f6e9c7a746d
mo k <mo@mokhan.ca>
2012-12-29 11:10:17 -0700

    cleanup spec

--- spec/specs/commands/dashboard/index_command_spec.rb
+++ spec/specs/commands/dashboard/index_command_spec.rb
@@ -4,28 +4,13 @@ module Booty
       let(:sut) { IndexCommand.new }
       context "when a request for the root of the website is received" do
         context "when it's for the root of the website" do
-          let(:request) do 
-            {
-              "REQUEST_PATH"=>"/",
-              "REQUEST_URI"=>"/",
-              "QUERY_STRING"=>""
-            }
-          end
-          before :each do
-            @result = sut.matches(request)
-          end
+          let(:request) { { "REQUEST_PATH"=>"/" } }
           it "should indicate that it can process the request" do
-            @result.should be_true
+            sut.matches(request).should be_true
           end
         end
         context "when it is for another path" do
-          let(:request) do
-            {
-              "REQUEST_PATH"=>"/hi/mo/1",
-              "REQUEST_URI"=>"/hi/mo/1?name=mo",
-              "QUERY_STRING"=>"name=mo"
-            }
-          end
+          let(:request) { { "REQUEST_PATH"=>"/hi/mo/1" } }
           it "should indicate that it cannot process the request" do
             sut.matches(request).should be_false
           end
