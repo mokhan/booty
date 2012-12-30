c43409b697affad29dd0b1fdadf3369fad349e6f
mo k <mo@mokhan.ca>
2012-12-30 08:38:13 -0700

    move view engine root_path to the constructor

--- lib/view_engine.rb
+++ lib/view_engine.rb
@@ -3,8 +3,11 @@ require 'expose_binding_behaviour'
 
 module Booty
   class ViewEngine
+    def initialize(defaults = {})
+      @defaults = defaults
+    end
     def render(options = {})
-      filename = File.join(options[:root_path], options[:template])
+      filename = File.join(@defaults[:root_path], options[:template])
       ERB.new(File.read(filename)).result(model_from(options).get_binder).chomp
     end
     private 
--- spec/specs/view_engine_spec.rb
+++ spec/specs/view_engine_spec.rb
@@ -3,11 +3,11 @@ require 'ostruct'
 
 module Booty
   describe ViewEngine do
-    let(:sut) { ViewEngine.new }
+    let(:sut) { ViewEngine.new(:root_path => 'spec/specs/views/') }
 
     context "when rendering a template" do
       before :each do
-        @result = sut.render(:root_path => 'spec/specs/views/', :template => "/view_engine.html.erb", :model => OpenStruct.new(:name => 'mo'))
+        @result = sut.render(:template => "/view_engine.html.erb", :model => OpenStruct.new(:name => 'mo'))
       end
       it "should convert the template to html" do
         @result.should == "<html>mo</html>"
