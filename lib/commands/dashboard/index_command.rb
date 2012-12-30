f79da1c0284372dbc76efa7d511deb4affdce2e0
mo k <mo@mokhan.ca>
2012-12-30 07:16:22 -0700

    implement the dashboard/index command.

--- lib/commands/dashboard/index_command.rb
+++ lib/commands/dashboard/index_command.rb
@@ -1,13 +1,14 @@
 module Booty
   module Dashboard
     class IndexCommand 
-      def initialize
-
+      def initialize(view_engine)
+        @view_engine = view_engine
       end
       def matches(request)
         request["REQUEST_PATH"] == "/"
       end
       def run_against(request)
+        [200, {"Content-Type" => "text/html"}, [@view_engine.render("/dashboard/index.html.erb")]]
       end
     end
   end
--- spec/specs/commands/dashboard/index_command_spec.rb
+++ spec/specs/commands/dashboard/index_command_spec.rb
@@ -1,7 +1,8 @@
 module Booty
   module Dashboard
     describe IndexCommand do
-      let(:sut) { IndexCommand.new }
+      let(:sut) { IndexCommand.new(view_engine) }
+      let(:view_engine) { fake }
       context "when a request for the root of the website is received" do
         context "when it's for the root of the website" do
           let(:request) { { "REQUEST_PATH"=>"/" } }
@@ -14,6 +15,18 @@ module Booty
           it "should indicate that it cannot process the request" do
             sut.matches(request).should be_false
           end
+        end
+      end
+      context "when rendering the home page" do
+        let(:html) { "<html></html>" }
+        before :each do
+          view_engine.stub(:render).with("/dashboard/index.html.erb").and_return(html)
+        end
+        before :each do
+          @result = sut.run_against(nil)
+        end
+        it "should display the correct view" do
+          @result.should == [200, {"Content-Type" => "text/html"}, [html]]
         end
       end
     end
