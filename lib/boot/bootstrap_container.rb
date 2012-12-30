99ad5541c5ccdd5048437f172e7ad19eb5515f71
mo k <mo@mokhan.ca>
2012-12-30 08:49:42 -0700

    register the new dashboard command.

--- lib/boot/bootstrap_container.rb
+++ lib/boot/bootstrap_container.rb
@@ -2,6 +2,8 @@ require 'container'
 require 'ioc'
 require 'front_controller'
 require 'command_registry'
+require 'view_engine'
+require 'index_command'
 
 container = Booty::Container.new
 container.register(:command_registry) do
@@ -9,6 +11,12 @@ container.register(:command_registry) do
 end
 container.register(:front_controller) do
   Booty::FrontController.new(container.resolve(:command_registry))
+end
+container.register(:view_engine) do
+  Booty::ViewEngine.new(:root_path => 'lib/views')
+end
+container.register(:http_command) do
+  Booty::Dashboard::IndexCommand.new(container.resolve(:view_engine))
 end
 
 Booty::IOC.bind_to(container)
