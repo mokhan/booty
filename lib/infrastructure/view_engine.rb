ea7955aeb63693493d3fa2349baa64de4587a37a
mo k <mo@mokhan.ca>
2013-01-02 12:47:27 -0700

    update view engine to be able to render content within a master page.

--- lib/infrastructure/view_engine.rb
+++ lib/infrastructure/view_engine.rb
@@ -7,14 +7,21 @@ module Booty
       @defaults = defaults
     end
     def render(options = {})
-      ERB.new(read_from_file(options[:template])).result(model_from(options).get_binder).chomp
+      html = ERB.new(read_from_file(options[:template])).result(model_from(options).get_binder).chomp
+      master_model = OpenStruct.new(:content => html)
+      master_model.extend(ExposeBindingBehaviour)
+
+      ERB.new(read_from_file(@defaults[:master])).result(master_model.get_binder).chomp
     end
+
     private 
+
     def model_from(options)
       model = options[:model]
       model.extend(ExposeBindingBehaviour)
       model
     end
+
     def read_from_file(template)
       File.read(File.join(@defaults[:root_path], template))
     end
--- spec/specs/infrastructure/view_engine_spec.rb
+++ spec/specs/infrastructure/view_engine_spec.rb
@@ -3,14 +3,14 @@ require 'ostruct'
 
 module Booty
   describe ViewEngine do
-    let(:sut) { ViewEngine.new(:root_path => 'spec/specs/views/') }
+    let(:sut) { ViewEngine.new(:root_path => 'spec/specs/views/', :master => 'master.html.erb') }
 
     context "when rendering a template" do
       before :each do
         @result = sut.render(:template => "/view_engine.html.erb", :model => OpenStruct.new(:name => 'mo'))
       end
       it "should convert the template to html" do
-        @result.should == "<html>mo</html>"
+        @result.should == "<html><head></head><body><p>mo</p></body></html>"
       end
     end
   end
