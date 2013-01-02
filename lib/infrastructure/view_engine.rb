ae4c69fe9ccb550139a3db89fab12c794dc0a3e3
mo k <mo@mokhan.ca>
2013-01-02 12:49:55 -0700

    clean up the view engine.

--- lib/infrastructure/view_engine.rb
+++ lib/infrastructure/view_engine.rb
@@ -7,19 +7,15 @@ module Booty
       @defaults = defaults
     end
     def render(options = {})
-      html = ERB.new(read_from_file(options[:template])).result(model_from(options).get_binder).chomp
-      master_model = OpenStruct.new(:content => html)
-      master_model.extend(ExposeBindingBehaviour)
-
-      ERB.new(read_from_file(@defaults[:master])).result(master_model.get_binder).chomp
+      html = ERB.new(read_from_file(options[:template])).result(binding_for(options[:model])).chomp
+      ERB.new(read_from_file(@defaults[:master])).result(binding_for(OpenStruct.new(:content => html))).chomp
     end
 
     private 
 
-    def model_from(options)
-      model = options[:model]
+    def binding_for(model)
       model.extend(ExposeBindingBehaviour)
-      model
+      model.get_binder
     end
 
     def read_from_file(template)
