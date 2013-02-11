3b4408583efeda0626525855ca4444e2fcd5e942
mo k <mo@mokhan.ca>
2013-02-11 12:45:33 -0700

    disconnect connection after use

--- lib/infrastructure/database/database_gateway.rb
+++ lib/infrastructure/database/database_gateway.rb
@@ -3,6 +3,9 @@ class DatabaseGateway
     @connection_factory = connection_factory
   end
   def run(command_or_query)
-    command_or_query.run(@connection_factory.create_connection)
+    connection = @connection_factory.create_connection
+    result = command_or_query.run(connection)
+    connection.disconnect
+    result
   end
 end
