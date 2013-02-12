4008ff67cd9865f120e54140ac0fc0fa6a693466
mo k <mo@mokhan.ca>
2013-02-11 19:01:29 -0700

    shuffle some files around

--- spec/specs/infrastructure/routing/request_specification_spec.rb
+++ spec/specs/infrastructure/routing/request_specification_spec.rb
@@ -1,20 +1,5 @@
 require "spec_helper"
 
-class RouteRequestSpecification
-  def handles(route)
-    @options = route
-  end
-  def matches(request)
-    matches_path(request) && matches_verb(request)
-  end
-  private
-  def matches_path(request)
-    @options[:uri].match request["REQUEST_PATH"]
-  end
-  def matches_verb(request)
-    @options.has_key?(:verb) ? @options[:verb].to_s.upcase == request["REQUEST_METHOD"] : true
-  end
-end
 describe RouteRequestSpecification do
   let(:sut) { RouteRequestSpecification.new  }
 
