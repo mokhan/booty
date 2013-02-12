bbca2b16791398bfcc109d1b5994c9255bcf25de
mo k <mo@mokhan.ca>
2013-02-11 20:51:44 -0700

    mixin the routing module to declaratively declare the route.

--- lib/commands/dashboard/index_command.rb
+++ lib/commands/dashboard/index_command.rb
@@ -1,15 +1,16 @@
 require 'http_command_behaviour'
+require 'routing_module'
 
 module Booty
   module Dashboard
     class IndexCommand 
       include HttpCommandBehaviour
+      include RoutingModule
+
+      handles :uri => /\/$/, :verb => :GET
 
       def initialize(view_engine)
         @view_engine = view_engine
-      end
-      def matches(request)
-        request["REQUEST_PATH"] == "/"
       end
       def run(request)
         html_response(@view_engine.render(:template => "/dashboard/index.html.erb"))
