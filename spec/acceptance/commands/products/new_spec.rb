856deb40e59b9b3849866dd69a06d0718fdaecce
mo khan <mo@mokhan.ca>
2013-02-12 19:14:14 -0700

    add acceptance test for rendering the /products/new page

---
+++ spec/acceptance/commands/products/new_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+require 'watir-webdriver'
+
+describe "/products/new" do
+  let(:sut) { Watir::Browser.new }
+  before :all do
+    sut.goto 'http://localhost:9292/products/new'
+  end
+  after :all do
+    sut.close
+  end
+  it "should say New" do
+    sut.text.include?('New Product').should be_true
+  end
+end
