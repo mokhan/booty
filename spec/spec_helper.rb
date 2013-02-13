d788accf969b94fe3c7fded66a76c0bd10a006d1
mo khan <mo@mokhan.ca>
2013-02-12 18:14:44 -0700

    re-arrange folders

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 require 'fakes-rspec'
-require_relative 'integration/infrastructure/test_database_gateway'
+require_relative 'integration/orm/test_database_gateway'
 Dir.glob("lib/**/*.rb").each { |x| $:.unshift(File.dirname(x)) }
 Dir.glob("lib/**/*.rb").each { |x| require File.basename(x) }
