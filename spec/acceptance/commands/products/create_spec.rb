004580e50c67f21356de413bdb1dce0b6c89a40b
mo k <mo@mokhan.ca>
2013-02-13 21:23:38 -0700

    add spec to create a new product.

--- spec/acceptance/commands/products/create_spec.rb
+++ spec/acceptance/commands/products/create_spec.rb
@@ -1,1 +1,14 @@
 require "acceptance_helper"
+
+describe '/products POST' do
+  navigate_to '/products/new'
+
+  before :each do
+    sut.text_field(:id => 'name').set 'blah'
+    sut.button(:name => 'submit').click
+  end
+
+  it "should be able to create a new product" do
+    sut.text.include? 'blah'
+  end
+end
