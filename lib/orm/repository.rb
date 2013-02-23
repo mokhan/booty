09c831fa8dfa978bc453833ab0d999da98b40815
mo khan <mo@mokhan.ca>
2013-02-23 13:17:00 -0700

    refactor repository

--- lib/orm/repository.rb
+++ lib/orm/repository.rb
@@ -13,31 +13,24 @@ class Repository
   end
 
   def save(item)
-    if item.id > 0
-      @database_gateway.run(prepare_update_command_for(item))
-    else
-      @database_gateway.run(prepare_insert_command_for(item))
-    end
+    @database_gateway.run( create_save_command_for(item) )
   end
 
   private
 
-  def prepare_update_command_for(item)
-    create_command { |connection| connection[@table].update(attributes_for(item)) }
-  end
+  def create_save_command_for(item)
+    return create_command do |connection| 
+      connection[@table].update(attributes_for(item)) 
+    end if item.id > 0
 
-  def prepare_insert_command_for(item)
     create_command do |connection|
-      item.instance_variable_set(:@id, connection[@table].insert(attributes_for(item).delete_if {|key, value| key == :id }))
+      id = connection[@table].insert(attributes_for(item).delete_if {|key, value| key == :id })
+      item.instance_variable_set(:@id, id)
     end
   end
 
   def attributes_for(item)
-    attributes = Hash.new
-    item.instance_variables.each do |variable|
-      attributes[variable.to_s.gsub(/@/, '').to_sym] = item.instance_variable_get(variable)
-    end
-    attributes
+    Hash[ item.instance_variables.map { |variable| [variable.to_s.gsub(/@/, '').to_sym, item.instance_variable_get(variable)] }]
   end
 
   def create_command
