fa0b170c120c00f69fefadb84fd03ea93745266f
mo khan <mo@mokhan.ca>
2013-02-25 21:17:58 -0700

    clean up the identity map lookup

--- lib/infrastructure/reload_file_strategy.rb
+++ lib/infrastructure/reload_file_strategy.rb
@@ -1,9 +1,25 @@
+require 'identity_map'
+
 class ReloadFileStrategy
-  def reload(item)
-    load(file_path_for(item))
+  def initialize(map = IdentityMap.new)
+    @identity_map = map
   end
+
+  def reload(item)
+    load(build_path_to(item))
+  end
+
   def file_path_for(item)
     command_path = item.to_s.gsub("Booty::", "").gsub("::","/").gsub("Command", "_Command").downcase + ".rb"
     File.expand_path(File.join(File.dirname(__FILE__), '../commands', command_path))
+  end
+
+  private
+
+  def build_path_to(item)
+    unless @identity_map.has_item_for?(item)
+      @identity_map.add(OpenStruct.new(:id => item, :path => file_path_for(item)))
+    end
+    @identity_map.item_for(item).path
   end
 end
