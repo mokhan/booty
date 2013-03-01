7bc22512a6ee856c7e8c299dc99282d620d0a5fa
mo khan <mo@mokhan.ca>
2013-02-28 20:40:19 -0700

    refactor database configuration to build a connectionstring and check for DATABASE_URL for heroku deployments.

--- lib/orm/database_configuration.rb
+++ lib/orm/database_configuration.rb
@@ -3,15 +3,22 @@ require 'pg'
 
 class DatabaseConfiguration
   def configure(connection)
-    configuration = load_all_configuration[current_environment]
-    connection.postgres(configuration["database"], :user => configuration["username"], :password => configuration["password"], :host => configuration['host'])
+    connection.connect(ENV['DATABASE_URL'] || build_connection_string)
   end
 
   private 
+
   def load_all_configuration
     YAML.load_file(File.join(File.dirname(__FILE__),'../../db/configuration.yml'))
   end
+
   def current_environment
     ENV["BOOTY_ENV"] || "test"
   end
+
+  def build_connection_string
+    configuration = load_all_configuration[current_environment]
+    "postgres://#{configuration['username']}:#{configuration['password']}@#{configuration['host']}/#{configuration['database']}"
+  end
+
 end
--- spec/specs/orm/database_configuration_spec.rb
+++ spec/specs/orm/database_configuration_spec.rb
@@ -2,13 +2,27 @@ require "spec_helper"
 
 describe DatabaseConfiguration do
   let(:sut) { DatabaseConfiguration.new }
+
   context "when configuring a new connection" do
     let(:connection) { fake }
+    let(:connection_string) { build_connection_string }
+
     before :each do
       sut.configure(connection)
     end
+
     it "should supply the proper settings" do
-      connection.should have_received(:postgres, 'booty_test', :user => 'booty', :password => 'password', :host => 'localhost')
+      connection.should have_received(:connect, connection_string)
     end
+
+    def load_all_configuration
+      YAML.load_file(File.join(File.dirname(__FILE__),'../../../db/configuration.yml'))
+    end
+
+    def build_connection_string
+      configuration = load_all_configuration[ENV["BOOTY_ENV"] || "test"]
+      "postgres://#{configuration['username']}:#{configuration['password']}@#{configuration['host']}/#{configuration['database']}"
+    end
+
   end
 end
