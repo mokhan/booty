4f20693e41fd4ab8bcf838af36cd58d2226c2882
mo khan <mo@mokhan.ca>
2013-02-28 22:44:58 -0700

    fix broken integration tests

--- spec/integration/orm/test_database_gateway.rb
+++ spec/integration/orm/test_database_gateway.rb
@@ -1,8 +1,22 @@
+require 'yaml'
+require 'pg'
+
 class TestDatabaseGateway
   def self.connection
-    @connection ||= Sequel.postgres('booty_test', :user => 'booty', :password => 'password', :host => 'localhost')
+    @connection ||= Sequel.connect(ENV['DATABASE_URL'] || build_connection_string)
   end
   def self.delete_all
     TestDatabaseGateway.connection.from(:products).delete
+  end
+
+  private
+
+  def self.build_connection_string
+    configuration = load_all_configuration[ENV["BOOTY_ENV"] || "test"]
+    "postgres://#{configuration['username']}:#{configuration['password']}@#{configuration['host']}/#{configuration['database']}"
+  end
+
+  def self.load_all_configuration
+    YAML.load_file(File.join(File.dirname(__FILE__),'../../../build/db/configuration.yml'))
   end
 end
