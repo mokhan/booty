367b9372558175dc3b809f389a66ebffc2040314
mo khan <mo@mokhan.ca>
2013-03-15 21:34:14 -0600

    test registering interceptors

--- spec/specs/infrastructure/containers/proxy_spec.rb
+++ spec/specs/infrastructure/containers/proxy_spec.rb
@@ -10,7 +10,21 @@ module Booty
         sut.greet('blah')
       end
       it "should send the message to the target" do
-        target.should have_received(:greet)
+        target.should have_received(:greet, 'blah')
+      end
+    end
+
+    context "when an interceptor is registered" do
+      context "when invoking a method" do
+        let(:interceptor) { fake }
+
+        before :each do
+          sut.add(:greet, interceptor)
+          sut.greet("blah")
+        end
+        it "should allow the interceptor to intercept the call" do
+          interceptor.should have_received(:intercept)
+        end
       end
     end
   end
