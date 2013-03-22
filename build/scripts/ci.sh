de7df463ea5b38b83096271333ddb61612f63c2e
mo khan <mo@mokhan.ca>
2013-03-21 22:03:39 -0600

    add ci config yml

--- build/scripts/ci.sh
+++ build/scripts/ci.sh
@@ -10,7 +10,7 @@ echo 'bundle install'
 bundle install
 
 echo 'cp db config'
-cp build/db/configuration.yml.template build/db/configuration.yml
+cp build/db/ci.configuration.yml build/db/configuration.yml
 
 echo 'run tests'
 bundle exec rake ci --trace
