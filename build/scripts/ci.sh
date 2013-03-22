8ca513aae7ec69949239eb31caa1d2ee8876d839
mo khan <mo@mokhan.ca>
2013-03-21 21:55:38 -0600

    tweak build script

--- build/scripts/ci.sh
+++ build/scripts/ci.sh
@@ -1,6 +1,15 @@
+#!/bin/bash -x
+echo 'loading bashrc'
 source ~/.bashrc
+
+echo 'loading gemset'
 cd .
 
+echo 'bundle install'
 bundle install
+
+echo 'cp db config'
 cp build/db/configuration.yml.template build/db/configuration.yml
+
+echo 'run tests'
 bundle exec rake ci --trace
