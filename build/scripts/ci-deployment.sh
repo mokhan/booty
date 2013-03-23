ba4482580b6de127252606f6ae727f02773e85f5
mo khan <mo@mokhan.ca>
2013-03-23 00:23:59 -0600

    load in rvm in deployment script

--- build/scripts/ci-deployment.sh
+++ build/scripts/ci-deployment.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
+echo 'loading bashrc'
+source ~/.bashrc
+
+echo 'loading gemset'
+#cd .
+rvm use ruby-2.0.0-p0@booty --create
+
 echo 'run deployment'
 bundle exec rake deploy --trace
