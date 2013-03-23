491e662ac081fb0d179b4bde6d3a8decf3ebc421
mo khan <mo@mokhan.ca>
2013-03-23 08:49:31 -0600

    try a 1 second sleep to see if that is why the acceptance build is failing in ci

--- spec/acceptance/commands/404_spec.rb
+++ spec/acceptance/commands/404_spec.rb
@@ -3,6 +3,9 @@ require "acceptance_helper"
 module Booty
   describe "/blah/blah" do
     navigate_to '/blah/blah/'
+    before :each do
+      sleep 1
+    end
 
     it "should say 404" do
       sut.text.include?('404 - Page Not Found').should be_true
