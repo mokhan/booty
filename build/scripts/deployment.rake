7a5dbfeddbd9e5d6a96f97dfd6a4f6d8388bc8fc
mo khan <mo@mokhan.ca>
2013-03-23 00:54:03 -0600

    uncomment out items that need to be fixed"

--- build/scripts/deployment.rake
+++ build/scripts/deployment.rake
@@ -1,7 +1,7 @@
 
 task :deploy do
   sh "git push heroku master"
-  #sh "heroku run rake db:migrate --app bootycall"
+  sh "heroku run rake db:migrate --app bootycall"
 end
 
 namespace :deploy do
--- spec/acceptance/commands/404_spec.rb
+++ spec/acceptance/commands/404_spec.rb
@@ -4,10 +4,10 @@ module Booty
   describe "/blah/blah" do
     navigate_to '/blah/blah/'
 
-    xit "should say 404" do
+    it "should say 404" do
       sut.text.include?('404 - Page Not Found').should be_true
     end
-    xit "should have a title" do
+    it "should have a title" do
       sut.title.should == '404 - Page Not Found - Booty'
     end
   end
