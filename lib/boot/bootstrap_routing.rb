ed37f71e602c9d19b88075a5184377385e9c02b3
mo khan <mo@mokhan.ca>
2013-03-23 20:50:12 -0600

    return a proxy to a route

--- lib/boot/bootstrap_routing.rb
+++ lib/boot/bootstrap_routing.rb
@@ -21,8 +21,7 @@ module Booty
     private
 
     def route_to(command)
-      Route.new(command, @container)
-      #proxy = Proxy.new(Route.new(command, @container))
+      proxy = Proxy.new(Route.new(command, @container))
       #proxy.add_interceptor(:run, UnitOfWorkInterceptor.new(UnitOfWorkFactory.new(SimpleContext.new,nil,Key.new("database.session")))
     end
   end
