6a484c3065df766adef670a0122d0250f2fcea92
mo khan <mo@mokhan.ca>
2013-03-24 22:13:49 -0600

    try to fix deployment script

--- build/scripts/ci-deployment.sh
+++ build/scripts/ci-deployment.sh
@@ -9,4 +9,6 @@ rvm use ruby-2.0.0-p0@booty --create
 echo 'run deployment'
 git checkout master
 git pull
-bundle exec rake deploy --trace
+git push heroku master
+heroku run rake db:migrate --app booty
+#bundle exec rake deploy --trace
