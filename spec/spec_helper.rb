6808c2695593cb87f67a0bcfade349553f66df8e
mo khan <mo@mokhan.ca>
2013-03-29 18:13:36 -0600

    require in fakes-rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ if ENV['CI']
   require 'simplecov'
   SimpleCov.start
 end
+require 'fakes-rspec'
 require_relative 'integration/orm/test_database_gateway'
 Dir.glob("lib/**/*.rb").each { |x| $:.unshift(File.dirname(x)) }
 Dir.glob("lib/**/*.rb").each { |x| require File.basename(x) }
