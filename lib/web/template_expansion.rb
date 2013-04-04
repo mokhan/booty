7f7230875cb7b06ac19e73cd03f8ca2607789cc5
mo khan <mo@mokhan.ca>
2013-04-03 21:37:40 -0600

    extract rendering logic from the view engine

---
+++ lib/web/template_expansion.rb
@@ -0,0 +1,23 @@
+require 'erb'
+require 'expose_binding_behaviour'
+
+module Booty
+  class TemplateExpansion
+    def initialize(root_path)
+      @root_path = root_path
+    end
+    def expand(file, model)
+      template = ERB.new(read_from(file))
+      binding = binding_for(model)
+      template.result(binding).chomp
+    end
+    private
+    def read_from(file)
+      File.read(File.join(@root_path, file))
+    end
+    def binding_for(model)
+      model.extend(ExposeBindingBehaviour)
+      model.get_binder
+    end
+  end
+end
--- lib/web/view_engine.rb
+++ lib/web/view_engine.rb
@@ -1,38 +1,16 @@
-require 'erb'
-require 'expose_binding_behaviour'
-
 module Booty
   class ViewEngine
-    def initialize(root_path: 'lib/commands', master: 'master.html.erb')
+    def initialize(root_path: 'lib/commands', master: 'master.html.erb', template_expander: TemplateExpansion.new(root_path))
       @root_path = root_path
       @master = master
+      @template_expander = template_expander
     end
 
     def render(template: '', model: OpenStruct.new )
       dto = OpenStruct.new(:model => model)
-      html = erb_from(template, dto).gsub(/\n/, '')
+      html = @template_expander.expand(template, dto).gsub(/\n/, '')
       dto.content = html
-      erb_from(@master, dto)
+      @template_expander.expand(@master, dto)
     end
-
-    private
-
-    def binding_for(model)
-      model.extend(ExposeBindingBehaviour)
-      model.get_binder
-    end
-
-    def read_from(file)
-      File.read(File.join(@root_path, file))
-    end
-
-    def erb_from(file, model)
-      template = ERB.new(read_from(file))
-      binding = binding_for(model)
-      template.result(binding).chomp
-    end
-  end
-  class TemplateExpansion
-
   end
 end
