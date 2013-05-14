e13ab1588090e02f1bed7e9f88f92123236ed738
mo khan <mo@mokhan.ca>
2013-05-13 20:09:31 -0600

    store password a little more securely

--- lib/domain/user.rb
+++ lib/domain/user.rb
@@ -1,11 +1,18 @@
 require "domain_object"
+require "bcrypt"
 
 class User < DomainObject
   def change_password(password)
-    @password = password
+    @password_hash = BCrypt::Password.create(password)
   end
 
   def password_matches(other_password)
-    @password == other_password
+    password == other_password
+  end
+
+  private 
+
+  def password
+    BCrypt::Password.new(@password_hash)
   end
 end
