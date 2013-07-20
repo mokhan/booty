de001277b32fe5ae843b34b79974915508d23e61
mo khan <mo@mokhan.ca>
2013-07-19 23:13:10 -0600

    fix broken spec

--- spec/integration/orm/mappings/product_mapping_spec.rb
+++ spec/integration/orm/mappings/product_mapping_spec.rb
@@ -3,18 +3,28 @@ require "spec_helper"
 describe "Product Mapping" do
   context "finding all products" do
     let(:product) { Product.new(:name => 'book') }
+
     before :each do
       connection_string = DatabaseConfiguration.new.connection_string
       configuration = Humble::Configuration.new(connection_string)
       configuration.add(ProductMapping.new)
       session_factory = configuration.build_session_factory
       @session = session_factory.create_session
+      @session.save(product)
+    end
+
+    after :each do
+      TestDatabaseGateway.delete_all
     end
 
     let(:results) { @session.find_all(Product) }
 
     it "should be able to load all Products" do
       results.should include(product)
+    end
+
+    it "should return the correct number of results" do
+      results.count.should == 1
     end
   end
 end
