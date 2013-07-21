60765a9feed86aab1e0bd7283437c65f08911ee7
mo khan <mo@mokhan.ca>
2013-07-20 20:44:02 -0600

    remove ci config expansion step

--- build/scripts/ci-unit.sh
+++ build/scripts/ci-unit.sh
@@ -8,9 +8,6 @@ rvm use ruby-2.0.0-p247@booty --create
 echo 'bundle install'
 bundle install
 
-echo 'cp db config'
-cp build/db/ci.configuration.yml build/db/configuration.yml
-
 echo 'run tests'
 export CI=true
 bundle exec rake spec:unit --trace
