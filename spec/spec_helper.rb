821a2a7ed1cefc36ff39bb1a84afe48ae8a35ed3
mo khan <mo@mokhan.ca>
2014-02-27 20:52:57 -0700

    require bundler and rubygems in spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ if ENV['CI']
   require 'simplecov'
   SimpleCov.start
 end
+require 'rubygems'
+require 'bundler'
 Bundler.require(:default, :test)
 require_relative 'integration/orm/test_database_gateway'
 require 'booty'
